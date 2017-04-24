a75533bad0b2aafc086bc0b9b52f54b3f6d0fb2b
Slava Monich <slava.monich@jolla.com>
2017-04-24 18:36:13 +0300

    Version 1.0.9

--- include/gofonoext_version.h
+++ include/gofonoext_version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Jolla Ltd.
+ * Copyright (C) 2015-2017 Jolla Ltd.
  * Contact: Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of BSD license as follows:
@@ -13,8 +13,8 @@
  *   2. Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of the Jolla Ltd nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
+ *   3. Neither the name of Jolla Ltd nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
  *      without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -37,7 +37,7 @@
 
 #define GOFONOEXT_VERSION_MAJOR   1
 #define GOFONOEXT_VERSION_MINOR   0
-#define GOFONOEXT_VERSION_RELEASE 8
+#define GOFONOEXT_VERSION_RELEASE 9
 
 #define GOFONOEXT_API_VERSION(major,minor,release) \
     (((major) << 24) | ((minor) << 16) | (release))
--- rpm/libgofonoext.spec
+++ rpm/libgofonoext.spec
@@ -1,5 +1,5 @@
 Name: libgofonoext
-Version: 1.0.8
+Version: 1.0.9
 Release: 0
 Summary: Client library for nemomobile ofono extensions
 Group: Development/Libraries
