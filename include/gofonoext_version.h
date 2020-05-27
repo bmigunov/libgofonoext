fef31d67f0ae11995cc7d1b4073ea39598796a65
Slava Monich <slava.monich@jolla.com>
2020-05-27 13:17:21 +0300

    [gofonoext] Freshened up license. JB#33828

--- include/gofonoext_version.h
+++ include/gofonoext_version.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015-2018 Jolla Ltd.
- * Contact: Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2015-2020 Jolla Ltd.
+ * Copyright (C) 2015-2020 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of BSD license as follows:
  *
@@ -13,9 +13,9 @@
  *   2. Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of Jolla Ltd nor the names of its contributors may
- *      be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -35,6 +35,8 @@
 
 #include "gofonoext_types.h"
 
+/* Since 1.0.2 */
+
 #define GOFONOEXT_VERSION_MAJOR   1
 #define GOFONOEXT_VERSION_MINOR   0
 #define GOFONOEXT_VERSION_RELEASE 10
@@ -47,8 +49,13 @@
                           GOFONOEXT_VERSION_MINOR,\
                           GOFONOEXT_VERSION_RELEASE)
 
+G_BEGIN_DECLS
+
 guint32
-ofonoext_version(void);
+ofonoext_version(
+    void);
+
+G_END_DECLS
 
 #endif /* GOFONOEXT_VERSION_H */
 
